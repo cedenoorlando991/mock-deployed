a93f8aa530b3640b27a4c09c3fbd35ff3598dba4
louiseweng <luyi_weng@brown.edu>
2023-10-11 16:08:17 -0400

    adding unit test

--- tests/REPLInput.spec.ts
+++ tests/REPLInput.spec.ts
@@ -10,7 +10,7 @@ test("should render REPLInput component initially", async ({ page }) => {
   expect(replInput).toBeTruthy();
 });
 
-test("should update   on input", async ({ page }) => {
+test("should update on input", async ({ page }) => {
   await page.fill("#root > div > div > div.repl-input > fieldset > input", "load_file test.csv");
   await page.click("#root > div > div > div.repl-input > button");
   const inputValue = await page.$eval("#root > div > div > div.repl-history > p > h4t",(el) => (el as HTMLInputElement).value
@@ -26,4 +26,16 @@ test("should update history on submit", async ({ page }) => {
     (el) => el.textContent
   );
   expect(historyItem).toBe("load_file test.csv");
+});
+
+test("should search history on submit", async ({ page }) => {
+  await page.fill(".repl-input input", "load_file test.csv");
+  await page.click(".repl-input button");
+  await page.fill(".repl-input input", "search");
+  await page.click(".repl-input button");
+  const historyItem = await page.$eval(
+    ".repl-input .history h4",
+    (el) => el.textContent
+  );
+  expect(historyItem).toBe("search");
 });
